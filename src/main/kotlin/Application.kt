3dcb31fda9c488938a78ff6f1497c285af1f3b23
Rupali Gangarde <rupaligangarde11@gmail.com>
2019-03-23 21:07:15 +0530

    Rupali | add JibaritoSandwichBuilder, fix typo

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,8 +1,8 @@
 fun main() {
     val clubSandwichBuilder = ClubSandwichBuilder()
-    val jibaritosandwichBuilder = JibaritoSandwichBuilder()
+    val jibaritoSandwichBuilder = JibaritoSandwichBuilder()
     val clubSandwich = clubSandwichBuilder.createSandwich()
-    val jibaritoSandwich = jibaritosandwichBuilder.createSandwich()
+    val jibaritoSandwich = jibaritoSandwichBuilder.createSandwich()
 
     print("Your Club sandwich contains ${clubSandwich.breadType} bread, ${clubSandwich.cheeseType} cheese and ${clubSandwich.vegetables} veggies, \n Thank you. Visit again \n")
     print("Your Jibarito sandwich contains ${jibaritoSandwich.breadType} bread, ${jibaritoSandwich.cheeseType} cheese and ${jibaritoSandwich.vegetables} veggies, \n Thank you. Visit again \n")
